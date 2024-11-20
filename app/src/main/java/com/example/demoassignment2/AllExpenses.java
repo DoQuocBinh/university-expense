79462b3d7fc00a6b5bdae9822cf6bcffbdc80fcb
DoQuocBinh <binhdq@fpt.edu.vn>
2024-11-20 15:31:55 +0700

    ok

--- app/src/main/java/com/example/demoassignment2/AllExpenses.java
+++ app/src/main/java/com/example/demoassignment2/AllExpenses.java
@@ -3,14 +3,19 @@ package com.example.demoassignment2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -32,6 +37,28 @@ public class AllExpenses extends AppCompatActivity {
                 R.layout.activity_listview, allExpense);
         ListView listView = (ListView) findViewById(R.id.listExpense);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ExpenseEntity entry = (ExpenseEntity) parent.getItemAtPosition(position);
+                final String[]  options = {"Delete","Update"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(AllExpenses.this);
+                builder.setItems(options,(dialog,item)->{
+                    if (options[item]=="Delete"){
+                        //1/Remove from the ListView
+                        allExpense.remove(position);
+                        adapter.notifyDataSetChanged();
+                        //2.Remove from database
+                        DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext());
+                        dbHelper.deleteExpense(entry.getId());
+                    }else if(options[item]=="Update"){
+                        Snackbar.make(view, "Update", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                });
+                builder.show();
+            }
+        });
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -51,4 +78,5 @@ public class AllExpenses extends AppCompatActivity {
         }
         return true;
     }
+
 }
